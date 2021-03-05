490cb2c979590d0b613e103dbdc65f532ca076ba
gitdevstar <56650395+gitdevstar@users.noreply.github.com>
2021-03-05 09:41:45 -0800

    add support ui

---
+++ wyretrade/wyretrade/Controllers/SupportController.swift
@@ -0,0 +1,19 @@
+//
+//  SupportController.swift
+//  wyretrade
+//
+//  Created by maxus on 3/5/21.
+//
+
+import Foundation
+import UIKit
+
+class SupportController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
